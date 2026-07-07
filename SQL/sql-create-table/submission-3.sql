76d4ce90c783bdb6f7bd41c2fd63ac38a7cc1cae
superpilot <124149769+vvinson849@users.noreply.github.com>
2026-07-07 23:54:25 +0800

    Add: sql-create-table - submission-3

---
+++ SQL/sql-create-table/submission-3.sql
@@ -0,0 +1,17 @@
+create table videos (
+    id integer,
+    name text,
+    created_at date,
+    published boolean
+);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
